d8cf9a0cf061c3743c0d7d56a07202333fc62d5f
Sudhir Yadav <yadavsudhir405@gmail.com>
2018-09-20 00:08:52 +0530

    Removed the copywebpack plugin

--- webpack-common.config.js
+++ webpack-common.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const entries = {
