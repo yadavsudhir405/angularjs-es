d3dd6cf82300879e18609a280e331d131efaf66b
Sudhir Yadav <yadavsudhir405@gmail.com>
2018-09-25 22:27:26 +0530

    Added css optimization plugin

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,8 +1,20 @@
 const merge = require('webpack-merge');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
 const commonConfigs = require('./webpack-common.config');
 
 const  prodConfigs = {
     mode: "production",
-    devtool: "source-map"
+    devtool: "source-map",
+    optimization: {
+        minimizer : [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }),
+            new OptimizeCssPlugin({})
+        ]
+    }
 };
 module.exports =  merge(commonConfigs, prodConfigs);
