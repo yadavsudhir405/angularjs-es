19a5b58ef91276ee4b8df40889df001e0b3e0295
Sudhir Yadav <yadavsudhir405@gmail.com>
2018-10-10 07:56:01 +0530

    Added Jquery functionality just to hide the button on click.

--- app/login/controller/LoginController.js
+++ app/login/controller/LoginController.js
@@ -1,8 +1,14 @@
+import $ from "jquery";
 class LoginController {
     constructor(constantService){
         this.message = "Login Page edited";
         this.constant = constantService.getConstant();
     }
+    handleClick() {
+        console.log("Clicked");
+        $("button.btn-primary").addClass("myclass");
+    }
+
 }
 LoginController.$inject = ['ConstantService'];
 export default LoginController;
