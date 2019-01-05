9f72bfc0e5852181fb00e4f610ce7565ccfb0d71
Sudhir Yadav <yadavsudhir405@gmail.com>
2019-01-05 19:49:17 +0530

    Added few contents to the demo page

--- app/demo/controller/DemoController.js
+++ app/demo/controller/DemoController.js
@@ -1,24 +1,46 @@
 import text from './greeting.txt';
+import img1 from '../../assets/images/webpack-es6.png';
+
 class DemoController {
-    constructor($window, $location){
+    constructor($window, $location) {
         this.greetingMessage = "Hello World";
-        this.text =  text;
+        this.text = text;
         this.show = false;
         this.$location = $location;
-        this.$window =  $window;
+        this.$window = $window;
         this.doRedirection();
+        this.img = img1;
+        this.speakersCount = [{
+            name: "Jerr Jack",
+            img: img1,
+            month: "January"
+        },
+            {
+                name: "Fog Joe",
+                img: img1,
+                month: "October"
+            },
+            {
+                name: "Frank Lee",
+                img: img1,
+                month: "December"
+            }
+        ]
     }
-    doRedirection(){
+
+    doRedirection() {
         let name = this.$location.search().name;
         /*if(name !== 'admin'){
             this.$location.path("/unathorized");
         }*/
     }
-    handleClick(){
+
+    handleClick() {
         console.log("Button Clicked");
         this.$location.path('/login');
     }
 }
-DemoController.$inject = ['$window','$location'];
+
+DemoController.$inject = ['$window', '$location'];
 
 export default DemoController;
