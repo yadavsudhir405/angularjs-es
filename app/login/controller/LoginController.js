2c41a007b916d80b727cd916f54bdac57e0bf286
Sudhir Yadav <yadavsudhir405@gmail.com>
2019-01-08 21:58:40 +0530

    Edited the login page with css

--- app/login/controller/LoginController.js
+++ app/login/controller/LoginController.js
@@ -1,8 +1,10 @@
 import $ from "jquery";
+import edsherran from "../../assets/images/login/edsheeran.png"
 class LoginController {
     constructor(constantService){
         this.message = "Login Page edited";
         this.constant = constantService.getConstant();
+        this.imageUrl = edsherran;
     }
     handleClick() {
         console.log("Clicked");
